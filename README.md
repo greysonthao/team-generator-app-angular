4ebc54fc7bfa39950bc6d936aa6cfd4df899af7e
Touger Thao <tougerthao@gmail.com>
2022-06-08 12:24:13 -0500

    first commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
 
+I followed the tutorial from [Laith Academy](https://www.youtube.com/watch?v=WHv1YQUg6ow) to create this Team Generator app.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numOfTeams: number | '' = '';
+  teams: string[][] = [];
+
+  addMember = () => {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Error: Name cannot be empty';
+      return console.log(this.errorMessage);
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  };
+
+  onInput = (inputValue: string) => {
+    this.newMemberName = inputValue;
+  };
+
+  onNumTeamsInput = (inputValue: string) => {
+    this.numOfTeams = Number(inputValue);
+
+    if (!this.numOfTeams) {
+      this.numOfTeams = '';
+    }
+
+    console.log('numOfTeams: ', this.numOfTeams);
+  };
+
+  generateTeams = () => {
+    if (this.numOfTeams <= 0 || !this.numOfTeams) {
+      this.errorMessage = 'Error: Number of teams cannot be <= 0';
+      return;
+    }
+    if (this.numOfTeams > this.members.length) {
+      this.errorMessage =
+        'Error: Number of teams cannot be more than than number of members';
+      return;
+    }
+    this.errorMessage = '';
+
+    const allMembers = [...this.members];
+
+    while (allMembers.length > 0) {
+      for (let i = 0; i < this.numOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+        if (!member) break;
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+        console.log(this.teams);
+      }
+    }
+
+    this.numOfTeams = '';
+    this.members = [];
+  };
 }
